c230f6c8b013d96a87c32b34df69f9ebdee2c848
NotAlex575 <dapontealessandro89gmail.com>
2024-12-12 15:35:07 +0100

    creato model, migration e seeder di technology

---
+++ database/migrations/2024_12_12_143111_create_technology_project_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('technology_project', function (Blueprint $table) {
+            $table->unsignedBigInteger("technology_id");
+            $table->foreign("technology_id")->references("id")->on("technology");
+
+            $table->unsignedBigInteger("project_id");
+            $table->foreign("project_id")->references("id")->on("project");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('technology_project', function (Blueprint $table) {
+            $table->dropForeign(['technology_id']);
+            $table->dropForeign(['project_id']);
+        });
+
+        Schema::dropIfExists('technology_project');
+    }
+};
